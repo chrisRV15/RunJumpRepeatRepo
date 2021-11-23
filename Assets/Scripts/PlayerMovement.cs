b103768fe18909b44934e6c6a509b756d942575f
chrisRV15 <christopher.d.thomasvillalobos@lions.enc.edu>
2021-11-22 22:00:15 -0500

    Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public CharacterController movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+
+        movement.Move(move * speed * Time.deltaTime);
+    }
+}
